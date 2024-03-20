73feecf93d275554091ddc7cc37787f3fa617161
Lucas Borges Corteletti <lucascorteletti@gmail.com>
2024-03-20 14:36:05 -0300

    Entregando a atividade POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone Nokia = new Nokia(numero: "987654", modelo: "Nokia 2289", imei: "abc", memoria: 4);
+Nokia.Ligar();
+Nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone Iphone = new Iphone(numero:"123456", modelo: "Iphone 15", imei: "541235", memoria: 256);
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("Telegram");
